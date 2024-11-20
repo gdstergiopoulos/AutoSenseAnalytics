5f5734790f84daef9cf888b1fe24d8b0322b28af
gdstergiopoulos <gdstergiopoulos@gmail.com>
2024-11-20 11:59:25 +0200

    improvements + about +contact and footers + db change

--- website/model/model.mjs
+++ website/model/model.mjs
@@ -1,12 +1,12 @@
 import db from 'better-sqlite3'
 import bcrypt from 'bcrypt'
 
-const sql= new db('model/hmtygrade.sqlite', { fileMustExist: true });
+const sql= new db('model/AutoSenseAnalytics.sqlite', { fileMustExist: true });
 
 export let registerUser = async (username, email,password) => {
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
-    let stmt = sql.prepare('INSERT INTO Account(username,email,password) VALUES(?,?,?)');
+    let stmt = sql.prepare('INSERT INTO User(username,email,password) VALUES(?,?,?)');
     try{
         stmt.run(username,email,hashedPassword);
         return true;
@@ -17,7 +17,7 @@ export let registerUser = async (username, email,password) => {
 }
 
 export let checkLogin = async (username, password) => {
-    let stmt = sql.prepare('SELECT username,password FROM Account WHERE username = ?');
+    let stmt = sql.prepare('SELECT username,password FROM User WHERE username = ?');
     try{
         let user = stmt.get(username);
         // console.log(user);
@@ -41,150 +41,4 @@ export let checkLogin = async (username, password) => {
         throw err;
     }
 }   
-
-export let getCourses= (semesterID) => {
-    let stmt = sql.prepare('SELECT * FROM Course WHERE semester = ?');
-    try{
-        let courses = stmt.all(semesterID);
-        return courses;
-    }
-    catch(err){
-        throw err;
-    }
-}
-
-export let getSelectedCourses = (studentID,semesterID) => {
-    let stmt = sql.prepare('SELECT C.courseName,C.courseCode,S.Semester,C.syntelestis\
-        FROM Course as C,Selection as S\
-        WHERE C.courseCode=S.courseCode AND S.username=? AND S.Semester=?');
-    try{
-        let courses = stmt.all(studentID,semesterID);
-        return courses;
-    }
-    catch(err){
-        throw err;
-    }
-}
-
-export let getSelectionCourses = () => {
-    let stmt = sql.prepare('SELECT courseName, syntelestis, courseCode FROM Course WHERE semester not in (1,2,3,4,5,6,10)');
-    try{
-        let courses = stmt.all();
-        // console.log(courses)
-        return courses;
-    }
-    catch(err){
-        throw err;
-    }
-}
-
-export let getAllCoreCourses = () => {
-    let stmt = sql.prepare('SELECT courseName, syntelestis FROM Course WHERE semester in (1,2,3,4,5,6,10)');
-    try{
-        let courses = stmt.all();
-        return courses;
-    }
-    catch(err){
-        throw err;
-    }
-}
-
-export let saveGrade = (username, courseCode, grade) => {
-    let stmt = sql.prepare('INSERT INTO Grades(student,course,grade) VALUES(?,?,?)');
-    try{
-        console.log("im here")
-        stmt.run(username,courseCode,grade);
-        return true;
-    }
-    catch(err){
-        throw err;
-    }
-}
-
-export let updateGrade = (username, courseCode, grade) => {
-    let stmt = sql.prepare('UPDATE Grades SET grade = ? WHERE student = ? AND course = ?');
-    try{
-        stmt.run(grade,username,courseCode);
-    }
-    catch(err){
-        throw err;
-    }
-}
-
-export let getCodes= () => {
-    let stmt = sql.prepare('SELECT courseCode FROM Course');
-    try{
-        let codes = stmt.all();
-        return codes;
-    }
-    catch(err){
-        throw err;
-    }
-}
-
-export let getGrades = (username) => {
-    let stmt = sql.prepare('SELECT course,grade FROM Grades WHERE student = ?');
-    try{
-        let grades = stmt.all(username);
-        return grades;
-    }
-    catch(err){
-        throw err;
-    }
-}
-
-export let getSpecificGrade=  (username, courseCode) => {
-    let stmt = sql.prepare('SELECT grade FROM Grades WHERE student = ? AND course = ?');
-    try{
-        let grade = stmt.all(username,courseCode);
-        return grade;
-    }
-    catch(err){
-        throw err;
-    }
-}
-
-export let getAllStudentCodes = (username) => {
-    let stmt = sql.prepare('SELECT DISTINCT courseCode FROM Course  WHERE semester in (1,2,3,4,5,6,10) UNION SELECT DISTINCT courseCode FROM Selection WHERE username=?');
-    try{
-        let courses = stmt.all(username);
-        return courses;
-    }
-    catch(err){
-        throw err;
-    }
-}
-
-export let getAllGradedCourses = (username) => {
-    let stmt = sql.prepare('SELECT course FROM Grades WHERE student = ?');
-    try{
-        let courses = stmt.all(username);
-        return courses;
-    }
-    catch(err){
-        throw err;
-    }
-}
-
-export let checkUser= async (username) => {
-    let stmt = sql.prepare('SELECT username FROM Account WHERE username = ?');
-    try{
-        let user = stmt.all(username);
-        return user;
-    }
-    catch(err){
-        throw err;
-    }
-}
-
-export let checkEmail= async (email) => {
-    let stmt = sql.prepare('SELECT email FROM Account WHERE email = ?');
-    try{
-        let user = stmt.all(email);
-        return user;
-    }
-    catch(err){
-        throw err;
-    }
-}
 
---
+++ website/public/src/map.js
@@ -0,0 +1,65 @@
+// Ensure that the HTML element with id "map" exists
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Ensure that the HTML element with id "map" exists
+    var mapElement = document.getElementById("map");
+    if (!mapElement) {
+        console.error("Element with id 'map' not found.");
+        return;
+    }
+
+    var map = L.map("map").setView([38.29, 21.7946], 14);
+
+    
+    custom=L.ExtraMarkers.icon({
+        icon: 'fa-number',
+        markerColor: 'cyan',
+        shape: 'circle',
+        prefix: 'fa'
+      });
+    var marker = L.marker([38.28864841960415, 21.788658751750393],{icon:custom}).addTo(map);
+    marker.bindPopup("AutoSense").addEventListener(this.onclick, function() {
+        marker.bindPopup("AutoSense").openPopup();});
+
+        var layer1=L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoiZ2RzdGVyZ2lvcG91bG9zIiwiYSI6ImNsdW1wdWxhYzB4ZmkyaWxuaDFjZjhoYnUifQ.M331BKPLXLd5K1jl6nFHcQ'
+          }).addTo(map);
+        
+        var layer2 = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: 'mapbox/dark-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: 'pk.eyJ1IjoiZ2RzdGVyZ2lvcG91bG9zIiwiYSI6ImNsdW1wdWxhYzB4ZmkyaWxuaDFjZjhoYnUifQ.M331BKPLXLd5K1jl6nFHcQ'
+        }).addTo(map);
+            
+        
+          var layer3=L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/satellite-v9',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoiZ2RzdGVyZ2lvcG91bG9zIiwiYSI6ImNsdW1wdWxhYzB4ZmkyaWxuaDFjZjhoYnUifQ.M331BKPLXLd5K1jl6nFHcQ'
+          }).addTo(map); 
+
+        baseMap = {
+            "Mapbox Satellite": layer3,
+            "Mapbox Streets": layer1,
+            "Mapbox Dark": layer2,
+
+        };
+
+        L.control.layers(baseMap).addTo(map);
+});
+
+    
+
+
+
