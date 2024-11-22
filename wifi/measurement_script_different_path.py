fb33c4a110c34ec45da0acea52ee6cb2d35215f3
esakematsi <esakellarioumatsi@gmail.com>
2024-11-22 13:41:31 +0200

    upgarded script

--- wifi/measurement_script_different_path.py
+++ wifi/measurement_script_different_path.py
@@ -5,30 +5,6 @@ from SCRIPT_rssi_bssd_single import get_wifi_measurement
 # FIWARE Orion Context Broker URL
 fiware_url = "http://150.140.186.118:1026/v2/entities"
 
-# Fake measurement data for the first post 
-testmeasurement = {
-    "id": "elenishome",
-    "type": "rssi_bssid",
-    "rssi": {
-        "value": -45,
-        "type": "Number"
-    },
-    "macAddress": {
-        "value": "00:14:22:01:23:45",
-        "type": "Text"
-    },
-    "location": {
-    "value": {
-        "type": "Point",
-        "coordinates": [21.753150, 38.230462]
-    },
-    "type": "geo:json"
-},
-    "timestamp": {
-        "value": datetime.now().isoformat(),
-        "type": "DateTime"
-    }
-}
 
 # Headers for the request
 headers = {
@@ -70,13 +46,6 @@ def post_to_fiware(measurement,fiware_url=fiware_url, headers=headers):
     except requests.exceptions.HTTPError as err:
         print(f"Failed to post measurement: {err}")
 
-    # response = requests.post(fiware_url, headers=headers, json=measurement)
-
-    # Check the response
-    # if response.status_code == 201:
-    #     print("Measurement posted successfully.")
-    # else:
-    #     print(f"Failed to post measurement. Status code: {response.status_code}, Response: {response.text}")
     return 0
 
 def patch_measument( measurement,fiware_url=fiware_url, headers=headers): #for uploading the measurements
@@ -90,34 +59,13 @@ def patch_measument( measurement,fiware_url=fiware_url, headers=headers): #for u
     return 0
 
 def wifi_measurement_loop(interval=10):
-    post_to_fiware(testmeasurement,fiware_url=fiware_url, headers=headers) 
+    #post_to_fiware(testmeasurement,fiware_url=fiware_url, headers=headers) 
     #change if you want to change the service path
     while True:
         bssid, rssi,timestamp,location = get_wifi_measurement()
 
         print(f"BSSID: {bssid}, RSSI: {rssi} dBm, Timestamp: {timestamp}")
-        # currmeasure = create_json(bssid, rssi, timestamp, location)
-        currmeasure = {
-            "rssi": {
-                "value": rssi,
-                "type": "Number"
-            },
-            "macAddress": {
-                "value": bssid,
-                "type": "Text"
-            },
-            "location": {
-            "value": {
-                "type": "Point",
-                "coordinates": location
-            },
-            "type": "geo:json"
-                },
-            "timestamp": {
-                "value": timestamp,
-                "type": "DateTime"
-            }
-        }
+        currmeasure = create_json(bssid, rssi, timestamp, location)
         if currmeasure:
             patch_measument(currmeasure,fiware_url+"/elenishome/attrs", headers)
         else:
@@ -126,7 +74,5 @@ def wifi_measurement_loop(interval=10):
         time.sleep(interval)
     
 
-
-#patch_measument(fiware_url=fiware_url+"/WiFiMeasurement/attrs", headers=headers, measurement=measurement2)
 
 wifi_measurement_loop(10)
