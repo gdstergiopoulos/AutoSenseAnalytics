9eca2ce0961868cf4e018f3f2ef8f661576cdd88
gdstergiopoulos <gdstergiopoulos@gmail.com>
2024-12-12 19:40:48 +0200

    write the existing data from MySQL to influxDB (no check)

--- backend_controller/LoRa/sync_fiware_influx_async.py
+++ backend_controller/LoRa/sync_fiware_influx_async.py
@@ -1,6 +1,11 @@
 import mysql.connector
 import json
 import requests
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+import pytz
+from datetime import datetime
+
 
 #Connection Details
 
@@ -16,8 +21,13 @@ db_config = {
 
 table_name = "AutoSenseAnalytics_LoRa_LoraMeasurement_rssi"
 
-#FIWARE ORION CB Connection Details
+#InfluxDB Connection Details
+influxdb_url = "http://150.140.186.118:8086"
+bucket = "AutoSenseAnalytics_LoRa"
+org = "students"
+token = "gyQjt9HYA8gzzQG1EjpW8GZV7RtbYBulx6UuCLRZUbqCagI9FR5FziuKy4CRIfTS-MY8t_z_-8g70v1ldFuriw=="
 
+#FIWARE ORION CB Connection Details
 fiware_url = "http://150.140.186.118:1026/v2/entities/LoraMeasurement/attrs"
 
 fiware_headers = {
@@ -84,7 +94,51 @@ def process_data(data):
         print(f"Error processing data: {e}")
         return None
 
-    
+#function to convert the timestamp to UTC
+def convert_to_utc(timestamp):
+        try:
+            # Define the Athens timezone
+            athens_tz = pytz.timezone('Europe/Athens')
+            
+            # Parse the timestamp string into a datetime object
+            local_time = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
+            
+            # Localize the datetime object to Athens timezone
+            local_time = athens_tz.localize(local_time)
+            
+            # Convert the localized time to UTC
+            utc_time = local_time.astimezone(pytz.utc)
+            
+            # Return the UTC time in the same format
+            #print(utc_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ"))
+            return utc_time.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        
+        except Exception as e:
+            print(f"Error converting timestamp to UTC: {e}")
+            return None   
+
+def update_influxdb(points):
+    client = InfluxDBClient(url=influxdb_url,bucket=bucket, token=token, org=org)
+    try:
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+        write_api.write(bucket=bucket, org=org, record=points)
+        write_api.flush()
+    finally:
+        client.close()
+        print("InfluxDB updated successfully")
+
+    return 0
+
 
 mysql_data=fetch_data(table_name,["rssi","location","timestamp"])
-print(mysql_data)
+points=[]
+for i in mysql_data:
+    point = Point("LoraMeasurement") \
+        .tag("LoRa", "Uni") \
+        .field("rssi", float(i["rssi"]))\
+        .field("latitude", float(i["latitude"]))\
+        .field("longitude", float(i["longitude"]))\
+        .time(convert_to_utc(i["timestamp"]))
+    points.append(point)
+
+update_influxdb(points)
