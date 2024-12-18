4164ab3f978a33ddd8bc2d581c5a923f03bb5a86
esakematsi <esakellarioumatsi@gmail.com>
2024-12-18 13:32:44 +0200

    arduino_counter

--- arduino/with_counter.c
+++ arduino/with_counter.c
@@ -1,4 +1,4 @@
-/*
+*
   Replace NWKSKEY, APPSKEY, DEVADDR
 
   Reference: https://github.com/goodcheney/ttn_mapper/blob/master/gps_shield
@@ -16,6 +16,7 @@ SoftwareSerial ss(3, 4); // Arduino RX (D3) <-> GPS_TX, Arduino TX (D4) <-> GPS_
 static void smartdelay(unsigned long ms);
 
 unsigned int count = 1; // Times counter
+uint8_t send_counter = 0;
 
 String datastring1 = "";
 String datastring2 = "";
@@ -27,22 +28,28 @@ char gps_lon[20] = {"\0"}; // Longitude
 char gps_alt[20] = {"\0"}; // Altitude
 float flat, flon, falt;
 
-int rssi_value = -255; // Default RSSI value (updated dynamically during events)
-
 static uint8_t mydata[] = "Hello, world!"; // For testing purposes
 
 /*
   LoRaWAN NwkSKey, network session key
+  This is the default Semtech key, which is used by the prototype TTN
+  network initially.
 */
+
+/*ac9860d5b896430f7a07be769b2a40e5*/
 static const PROGMEM u1_t NWKSKEY[16] = { 0xac, 0x98, 0x60, 0xd5, 0xb8, 0x96, 0x43, 0x0f, 0x7a, 0x07, 0xbe, 0x76, 0x9b, 0x2a, 0x40, 0xe5 };
 
 /*
   LoRaWAN AppSKey, application session key
+  This is the default Semtech key, which is used by the prototype TTN
+  network initially.
 */
+/*c11733c52d513c3540ded2f05f49c6e6*/
 static const u1_t PROGMEM APPSKEY[16] = { 0xc1, 0x17, 0x33, 0xc5, 0x2d, 0x51, 0x3c, 0x35, 0x40, 0xde, 0xd2, 0xf0, 0x5f, 0x49, 0xc6, 0xe6 };
 
 /*
   LoRaWAN end-device address (DevAddr)
+  See http://thethingsnetwork.org/wiki/AddressSpace
 */
 static const u4_t DEVADDR = 0x260111D1;
 
@@ -61,7 +68,7 @@ static osjob_t initjob, sendjob, blinkjob;
   Schedule TX every this many seconds (might become longer due to duty
   cycle limitations).
 */
-const unsigned TX_INTERVAL = 10;
+const unsigned TX_INTERVAL = 0.1;
 
 // Pin mapping
 const lmic_pinmap lmic_pins = {
@@ -71,30 +78,33 @@ const lmic_pinmap lmic_pins = {
   .dio = {2, 6, 7}
 };
 
-void do_send(osjob_t* j) {
+void do_send(osjob_t* j){
   // Check if there is not a current TX/RX job running
   if (LMIC.opmode & OP_TXRXPEND) {
     Serial.println("OP_TXRXPEND, not sending");
   } else {
     GPSRead();
     GPSWrite();
+    
+    Serial.print("Counter value: ");
+    Serial.println(send_counter);
 
     // Prepare upstream data transmission at the next possible time.
-    // Embed RSSI value into the payload
-    datasend[6] = rssi_value; // Add RSSI value to the payload
-
     LMIC_setTxData2(1, datasend, sizeof(datasend) - 1, 0);
+    // LMIC_setTxData2(1, mydata, sizeof(mydata) - 1, 0);
     Serial.println("Packet queued");
     Serial.print("LMIC.freq:");
     Serial.println(LMIC.freq);
     Serial.println("");
     Serial.println("");
     Serial.println("Receive data:");
-  }
+
+    send_counter = (send_counter + 1) % 256;
+  } 
   // Next TX is scheduled after TX_COMPLETE event.
 }
 
-void onEvent(ev_t ev) {
+void onEvent (ev_t ev) {
   Serial.print(os_getTime());
   Serial.print(": ");
   Serial.println(ev);
@@ -133,12 +143,6 @@ void onEvent(ev_t ev) {
         Serial.print("Data Received: ");
         Serial.write(LMIC.frame + LMIC.dataBeg, LMIC.dataLen);
         Serial.println();
-
-        // Update RSSI value
-        rssi_value = LMIC.rssi;
-        Serial.print("Updated RSSI Value: ");
-        Serial.print(rssi_value);
-        Serial.println(" dBm");
       }
       // Schedule next transmission
       os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
@@ -152,12 +156,6 @@ void onEvent(ev_t ev) {
     case EV_RXCOMPLETE:
       // Data received in ping slot
       Serial.println("EV_RXCOMPLETE");
-
-      // Update RSSI value
-      rssi_value = LMIC.rssi;
-      Serial.print("Updated RSSI Value: ");
-      Serial.print(rssi_value);
-      Serial.println(" dBm");
       break;
     case EV_LINK_DEAD:
       Serial.println("EV_LINK_DEAD");
@@ -178,19 +176,35 @@ void setup() {
   while (!Serial) {};
   Serial.println("LoRa GPS Example---- ");
   Serial.println("Connect to TTN");
+  #ifdef VCC_ENABLE
+    // For Pinoccio Scout boards
+    pinMode(VCC_ENABLE, OUTPUT);
+    digitalWrite(VCC_ENABLE, HIGH);
+    delay(1000);
+  #endif
 
   // LMIC init
   os_init();
   // Reset the MAC state. Session and pending data transfers will be discarded.
   LMIC_reset();
-
+  /*
+    LMIC_setClockError(MAX_CLOCK_ERROR * 1/100);
+    Set static session parameters. Instead of dynamically establishing a session
+    by joining the network, precomputed session parameters are be provided.
+  */
   #ifdef PROGMEM
+    /*
+      On AVR, these values are stored in flash and only copied to RAM
+      once. Copy them to a temporary buffer here, LMIC_setSession will
+      copy them into a buffer of its own again.
+    */
     uint8_t appskey[sizeof(APPSKEY)];
     uint8_t nwkskey[sizeof(NWKSKEY)];
     memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
     memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
     LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
   #else
+    // If not running an AVR with PROGMEM, just use the arrays directly 
     LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
   #endif
 
@@ -222,18 +236,43 @@ void GPSWrite() {
   // Convert GPS data to format
   datastring1 += dtostrf(flat, 0, 4, gps_lat);
   datastring2 += dtostrf(flon, 0, 4, gps_lon);
+  // datastring3 += dtostrf(falt, 0, 2, gps_alt);
 
   if (flat != 1000.000000) {
     strcat(gps_lat, ",");
     strcat(gps_lat, gps_lon);
+    // strcat(gps_lat, ",");
+    // strcat(gps_lat, gps_alt);
+    int i = 0;
+    for (i = 0; i < 2; i++) {
+      // datasend.toFloat();
+      atof(gps_lat);
+      // Serial.println((char*)datasend);
+      Serial.println("Testing converted data:");
+      Serial.println(gps_lat);
+      // atof(gps_alt);
+      // Serial.print(gps_alt);
+    }
 
-    strcpy((char*)datasend, gps_lat); // The format of datasend is latitude, longtitude
-    Serial.print("The latitude and longtitude are:");
+    strcpy((char*)datasend, gps_lat); // The format of datasend is latitude, longtitude, altitude
+    Serial.print("###########    ");
+    Serial.print("NO.");
+    Serial.print(count);
+    Serial.println("    ###########");
+    Serial.println("The latitude and longtitude are:");
     Serial.print("[");
     Serial.print((char*)datasend);
     Serial.print("]");
     Serial.println("");
-
+    /*
+      for (int k = 0; k < 20; k++) {
+        Serial.print("[");
+        Serial.print(datasend[k], HEX);
+        Serial.print("]");
+      }
+      Serial.println("");
+      Serial.println("");
+    */
     count++;
   }
 
@@ -247,7 +286,7 @@ void GPSWrite() {
   datasend[3] = lon;
   datasend[4] = lon >> 8;
   datasend[5] = lon >> 16;
-
+  datasend[6]=send_counter; 
   smartdelay(1000);
 }
 
