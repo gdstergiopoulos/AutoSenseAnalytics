62084e4e69882f625cf3fd72f49d12fb9aaa588c
gdstergiopoulos <gdstergiopoulos@gmail.com>
2025-02-03 20:09:41 +0200

    make it a func and create subscription for 4g rssi

--- wifi/sub_fiware_mqtt.py
+++ wifi/sub_fiware_mqtt.py
@@ -3,35 +3,69 @@ from requests.structures import CaseInsensitiveDict
 
 url = "http://150.140.186.118:1026/v2/subscriptions"
 
-headers = CaseInsensitiveDict()
-headers["Content-Type"] = "application/json"
-headers["Fiware-ServicePath"] = "/AutoSenseAnalytics/demo"
 
-data = """
-{ 
-  "description": "TEST2", 
-  "subject": { 
-    "entities": [ 
-      { 
-        "id": "car0", 
-        "type": "car_measurements" 
-      } 
-    ],
-    "condition": {
-      "attrs": [
-        "timestamp"
-      ]
-    }
-  }, 
-  "notification": {
-    "mqtt": {
-      "url": "mqtt://150.140.186.118:1883",
-      "topic": "autosense/demo"
-    }
-  }
-}
-"""
 
-resp = requests.post(url, headers=headers, data=data)
-print(resp.status_code)
-print(resp.text)
+def subscribe_entity_to_mqtt(fiware_service_path, entity_id,entity_type,update_attr,mqtt_topic):
+    headers = CaseInsensitiveDict()
+    headers["Content-Type"] = "application/json"
+    headers["Fiware-ServicePath"] = "%s"%fiware_service_path  
+    data="""{
+                "description": "subscription for entity %s",
+                "subject": {
+                    "entities": [
+                        {
+                            "id": "%s",
+                            "type": "%s"
+                        }
+                    ],
+                    "condition": {
+                        "attrs": [
+                            "%s"
+                        ]
+                    }
+                },
+                "notification": {
+                    "mqtt": {
+                        "url": "mqtt://150.140.186.118:1883",
+                        "topic": "autosense/%s"
+                    }
+                }
+            }"""%(entity_id,entity_id,entity_type,update_attr,mqtt_topic)
+    response = requests.post(url, headers=headers, data=data)
+    print(response.status_code)
+    print(response.text)
+    
+#demo entity 
+#DO THIS FOR EVERY CAR (ONLY car0 exists)
+subscribe_entity_to_mqtt("/AutoSenseAnalytics/demo","car0","car_measurements","timestamp","demo")
+#4g entity
+subscribe_entity_to_mqtt("/AutoSenseAnalytics","4G_Measurement","4G","rssi","4g_rssi")
+
+# data = """
+# { 
+#   "description": "TEST2", 
+#   "subject": { 
+#     "entities": [ 
+#       { 
+#         "id": "car0", 
+#         "type": "car_measurements" 
+#       } 
+#     ],
+#     "condition": {
+#       "attrs": [
+#         "timestamp"
+#       ]
+#     }
+#   }, 
+#   "notification": {
+#     "mqtt": {
+#       "url": "mqtt://150.140.186.118:1883",
+#       "topic": "autosense/demo"
+#     }
+#   }
+# }
+# """
+
+# resp = requests.post(url, headers=headers, data=data)
+# print(resp.status_code)
+# print(resp.text)
